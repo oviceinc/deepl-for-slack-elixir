0245d9b6200232f08cb8a4b8d4f1aa75b7c0316c
Sedki BEN ZID <58634448+Sed9yDataBank@users.noreply.github.com>
2026-03-13 09:11:58 +0100

    Update authorization header for DeepL API requests
    Fix failed to translate due to an unexpected response from translation server.
    
    [Docs](https://developers.deepl.com/docs/getting-started/intro#http-request:~:text=Authorization%3A%20DeepL%2DAuth%2DKey%20%5ByourAuthKey%5D)

--- lib/deep_thought/deepl/api.ex
+++ lib/deep_thought/deepl/api.ex
@@ -1,15 +1,20 @@
 defmodule DeepThought.DeepL.API do
   @moduledoc """
-  Module used to interact with the DeepL translation API. In order to use functions defined in this module,
-  an environmental variable `DEEPL_AUTH_KEY` (which gets loaded into application config) is required, containing the
-  value of the DeepL auth key generated in your DeepL Pro account. Currently, the paid version of the DeepL API is
-  always called, which doesn’t work with free auth key.
+  Module used to interact with the DeepL translation API.
+
+  Requires `DEEPL_AUTH_KEY` in application config (from env). Uses the paid DeepL API;
+  does not work with a free auth key.
   """
 
   use Tesla
 
+  @auth_key Application.get_env(:deep_thought, :deepl)[:auth_key]
+
+  @headers [
+    {"Authorization", "DeepL-Auth-Key #{@auth_key}"}
+  ]
+
   plug Tesla.Middleware.BaseUrl, "https://api.deepl.com/v2"
-  plug Tesla.Middleware.Query, auth_key: Application.get_env(:deep_thought, :deepl)[:auth_key]
   plug Tesla.Middleware.EncodeFormUrlencoded
   plug Tesla.Middleware.DecodeJson
   plug Tesla.Middleware.Logger
@@ -20,7 +25,8 @@ defmodule DeepThought.DeepL.API do
   """
   @spec translate(String.t(), String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def translate(text, target_language) do
-    {:ok, response} = post("/translate", translate_request_body(text, target_language))
+    {:ok, response} =
+      post("/translate", translate_request_body(text, target_language), headers: @headers)
 
     case response.status() do
       200 ->
@@ -32,11 +38,12 @@ defmodule DeepThought.DeepL.API do
   end
 
   @spec translate_request_body(String.t(), String.t()) :: %{String.t() => String.t()}
-  defp translate_request_body(text, target_language),
-    do: %{
+  defp translate_request_body(text, target_language) do
+    %{
       "text" => text,
       "target_lang" => target_language,
       "tag_handling" => "xml",
       "ignore_tags" => "c,d,e,l,u"
     }
+  end
 end
