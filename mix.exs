3f737e43a74e2f92d57aa817869329435b630f80
Sed9yDataBank <benzidsedki@gmail.com>
2026-03-26 10:08:53 +0100

    (fix): Update all deps and push latest lock file

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DeepThought.MixProject do
     [
       app: :deep_thought,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
